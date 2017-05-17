b7555d16abd778070fcb532c41c5479868ee5c01
Ajay David <antonio.g.david@gmail.com>
2017-05-17 14:39:05 +0300

    refactor to use specific ReactNative registries and cleanup function

--- index.ios.js
+++ index.ios.js
@@ -2,17 +2,16 @@
 
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+  // Access the specific registries of the entire ReactNative library
+import { Text, AppRegistry } from 'react-native';
 
 // Create a component
-const App = () => {
-  return (
-    // JSX - a dialect of JS that follows the semantics of HTML
-    // Method to communicate to React Native what should be displayed on mobile device
-    // Looks like HTML but translates into JS React function calls
-    <Text>Some Text</Text>
-  );
-};
+const App = () => (
+  // JSX - a dialect of JS that follows the semantics of HTML
+  // Method to communicate to React Native what should be displayed on mobile device
+  // Looks like HTML but translates into JS React function calls
+  <Text>Some Text</Text>
+);
 
 // Render it to the device
-ReactNative.AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('albums', () => App);
