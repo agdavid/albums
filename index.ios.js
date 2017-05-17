f8dab092d86479da64a3f671277f4ba3f529ad92
Ajay David <antonio.g.david@gmail.com>
2017-05-17 15:04:12 +0300

    import and nest Header in root

--- index.ios.js
+++ index.ios.js
@@ -4,13 +4,12 @@
 import React from 'react';
   // Access the specific registries of the entire ReactNative library
 import { Text, AppRegistry } from 'react-native';
+  // Access an exported component through relative path
+import Header from './src/components/header';
 
 // Create a component
 const App = () => (
-  // JSX - a dialect of JS that follows the semantics of HTML
-  // Method to communicate to React Native what should be displayed on mobile device
-  // Looks like HTML but translates into JS React function calls
-  <Text>Some Text</Text>
+  <Header />
 );
 
 // Render it to the device
