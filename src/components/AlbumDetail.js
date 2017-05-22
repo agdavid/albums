fc325f5ceeac9d004b55dcab6ae7a140a1a1f1d2
Ajay David <antonio.g.david@gmail.com>
2017-05-22 10:42:04 -0400

    style thumbnail container for more spacing

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,12 +5,16 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
@@ -33,6 +37,12 @@ const styles = {
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
   }
 };
 
